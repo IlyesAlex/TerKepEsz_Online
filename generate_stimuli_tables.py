1d781a0030355bcdede11a8754f5dc90c25301a2
Zsuzsanna Nemecz <nemecz.zsuzsanna@ttk.hu>
2020-05-15 00:09:53 +0200

    add more trial files to choose from

--- generate_stimuli_tables.py
+++ generate_stimuli_tables.py
@@ -6,7 +6,7 @@ import pandas as pd
 import trk_module as trk
 import os.path as op
 
-n_files = 2 # define number of stimuli files to be created
+n_files = 3 # define number of stimuli files to be created
 encoding_fname = 'StimuliTable-Encoding_3-runs_40-pairs_8-discrete-loc_12345-delays.xlsx'
 recognition_fname = 'StimuliTable-Recognition_3-runs_40-pairs_8-discrete-loc_12345-delays.xlsx'
 encoding_table = pd.read_excel(encoding_fname)
